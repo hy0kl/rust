8f2776ab97967b8ca5691bedd65cb66f583e2eb5
Jerry Yang <hy0kle@gmail.com>
2015-10-22 14:05:55 +0800

    some comments for write file. :hibiscus:

--- src/write.rs
+++ src/write.rs
@@ -6,7 +6,7 @@ fn main() {
     let mut f = std::fs::OpenOptions::new()
         .read(true)
         .write(true)
-        .create(true)
+        .create(true)   // 目标文件不存在时创建新文件.不加此选项,打开不存在文件时会报错
         .truncate(true) // 不加此行,如果文件存在,不清空原文件,会 seek 到文件件首部
         .open("foo.txt")
         .ok()
@@ -23,7 +23,7 @@ fn main() {
 
     let mut f = std::fs::OpenOptions::new()
         .write(true)
-        .append(true)
+        .append(true)   // 追加选项
         .open("foo.txt")
         .ok()
         .expect("Can NOT append data into file.");
