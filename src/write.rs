bf6b75acb91eb01f04f233c61f7e51f08a3c5578
Jerry Yang <hy0kle@gmail.com>
2015-10-22 12:42:53 +0800

    加入详细的文件操作说明.

--- src/write.rs
+++ src/write.rs
@@ -2,30 +2,35 @@ use std::io::Write;
 
 fn main() {
     println!("不存在文件时,创建,然后写数据;如果存在,清空后再写");
+
     let mut f = std::fs::OpenOptions::new()
         .read(true)
         .write(true)
         .create(true)
-        // .truncate(true)
+        .truncate(true) // 不加此行,如果文件存在,不清空原文件,会 seek 到文件件首部
         .open("foo.txt")
         .ok()
         .expect("Couldn’t open foo.txt");
-
     println!("{:?}", f);
 
-    let buf = b"whatever just study rust.\n";
+    let buf = b"Whatever just study rust.\n";
     let result = f.write(buf).ok().expect("Write fail.");
-
     println!("ret: {:?}", result);
 
+    // ---
+
     println!("追加内容");
+
     let mut f = std::fs::OpenOptions::new()
         .write(true)
         .append(true)
         .open("foo.txt")
         .ok()
         .expect("Can NOT append data into file.");
-    let buf = b"append some contents.\n";
-    let result = f.write(buf).ok().expect("Append contents fail.");
+    let buf = b"Append some contents.\n";
+    let result = f.write(buf)
+        .ok()
+        .expect("Append contents fail.");
     println!("append op ret: {}", result);
 }
+
