f0bdfa91d6e76e321a2fd3bfcf9c765156173515
Jerry Yang <hy0kle@gmail.com>
2015-10-22 15:33:40 +0800

    add some code for iterator

--- src/iterator.rs
+++ src/iterator.rs
@@ -14,4 +14,28 @@ fn main() {
     for i in v {
         println!("Take ownership of the vector and its element {}", i);
     }
+
+    let mut range = 0 .. 10;
+
+    loop {
+        match range.next() {
+            Some(x) => {
+                println!("current is: {}", x);
+            },
+                None => {break}
+        }
+    }
+
+    for x in 10 .. 20 {
+        println!(" -> {}", x);
+    }
+
+    let nums = vec![1, 2, 3];
+    for i in 0 .. nums.len() {
+        println!("index: {}, value: {}", i, nums[i]);
+    }
+
+    for num in &nums {
+        println!("value: {}", num);
+    }
 }
