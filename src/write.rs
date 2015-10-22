4b5e728329fecf3295b02199d1990aebd17ec766
Jerry Yang <hy0kle@gmail.com>
2015-10-22 12:30:32 +0800

    写文件参考源代码.

--- src/write.rs
+++ src/write.rs
@@ -1,11 +1,12 @@
 use std::io::Write;
 
 fn main() {
-    //let mut f = std::fs::File::open("foo.txt").write(true).ok().expect("Couldn’t open foo.txt");
+    println!("不存在文件时,创建,然后写数据;如果存在,清空后再写");
     let mut f = std::fs::OpenOptions::new()
         .read(true)
         .write(true)
         .create(true)
+        // .truncate(true)
         .open("foo.txt")
         .ok()
         .expect("Couldn’t open foo.txt");
@@ -16,4 +17,15 @@ fn main() {
     let result = f.write(buf).ok().expect("Write fail.");
 
     println!("ret: {:?}", result);
+
+    println!("追加内容");
+    let mut f = std::fs::OpenOptions::new()
+        .write(true)
+        .append(true)
+        .open("foo.txt")
+        .ok()
+        .expect("Can NOT append data into file.");
+    let buf = b"append some contents.\n";
+    let result = f.write(buf).ok().expect("Append contents fail.");
+    println!("append op ret: {}", result);
 }
