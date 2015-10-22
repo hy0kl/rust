f542e9a655a27301aacce737d2d1b224c92e70d5
Jerry Yang <hy0kle@gmail.com>
2015-10-22 15:56:48 +0800

    迭代器加入更多代码用于测试与参考.

--- src/iterator.rs
+++ src/iterator.rs
@@ -3,6 +3,9 @@ fn main() {
 
     let mut v = vec![1, 2, 3, 4, 5];
 
+    let v_len = v.len();
+    println!("v.len() = {}", v_len);
+
     for i in &v {
         println!("A reference to {}", i);
     }
@@ -11,6 +14,11 @@ fn main() {
         println!("A mutable reference to {}", i);
     }
 
+    for i in 0 .. v_len {
+        println!("as array v[{}] = {}", i, v[i]);
+    }
+
+    // 以下代码会影响 v 的作用域,上面3行代码放到 i in v 块后,编译不过.
     for i in v {
         println!("Take ownership of the vector and its element {}", i);
     }
