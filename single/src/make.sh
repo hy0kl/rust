da0e088bee84cef1e2af483456687ea6160267dd
yangyj <yangyj@jyb.com>
2016-08-15 15:41:41 +0800

    改进控制脚本

--- single/src/make.sh
+++ single/src/make.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 # @describe:
 # @author:   Jerry Yang(hy0kle@gmail.com)
 
@@ -6,7 +6,27 @@
 
 target="read-file write iterator exec dining-philosophers args string"
 
-for tag in $target
-do
-    rustc $tag.rs
-done
+argc=$#
+if ((argc < 1))
+then
+    echo "Usage: $0 (build|clean)"
+    exit 0
+fi
+
+action=$1
+
+if [ "build" = "$action" ]
+then
+    for tag in $target
+    do
+        rustc $tag.rs
+    done
+elif [ "clean" = "$action" ]
+then
+    for tag in $target
+    do
+        rm "$tag"
+    done
+else
+    echo "invalid action: $action"
+fi
